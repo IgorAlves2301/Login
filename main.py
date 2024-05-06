123c581f2410abcec5fc85996e911c691859633d
IgorAlves2301 <igoralvesdossantosfloriano@gmail.com>
2024-05-06 03:21:28 -0300

    Update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+
+
+
+
 # Define colors
 color1 = "Black"
 color2 = "LightBlue"
@@ -11,17 +15,26 @@ correct_password = "12345"
 correct_username = 'igor'
 
 # Function to verify login
-def confirm():
+def confirm(event=None):
     
     username_entered = entry_username.get()
     password_entered = entry_password.get()
     
     if username_entered == correct_username and password_entered == correct_password:
         open_main_page()
-    else:
+    elif username_entered == correct_username and password_entered != correct_password:
 
-        error_label.config(text="Login falhou, tente novamente")
+        error_label_password.config(text="Login falhou, tente outra senha")
         entry_password.delete(0, 'end')
+    elif username_entered != correct_username and password_entered != correct_password:
+        error_label_password.config(text="Login falhou, senha e nome de usuario errados")
+        entry_username.delete(0, 'end')
+        entry_password.delete(0, 'end')
+    else:
+        error_label_username.config(text="Login falhou, nome de usuario errado ")
+        entry_username.delete(0, 'end')
+        entry_username.focus()
+        
 
 # Function to open main page
 def open_main_page():
@@ -36,30 +49,36 @@ def open_main_page():
 # Login window
 root = Tk()
 root.title('Login')
-root.geometry('300x350')
+root.geometry('500x650')
 root.config(background=color3)
 root.resizable(width=False, height=False)
 
 
-login_label = Label(root, text='Login', fg=color2, font=('Arial', 40, 'bold'))
+login_label = Label(root, text='Login', fg=color2, font=('Arial', 40, 'bold'), bg=color3)
 login_label.place(x=20, y=20)
 
-username_text = Label(root, text='Nome:', fg=color1)
+username_text = Label(root, text='Nome:', font=('Arial', 15, 'bold'),fg=color1, bg=color3,)
 username_text.place(x=30, y=150)
-entry_username = Entry(root)
-entry_username.place(x=90, y=150)
+entry_username = Entry(root, width=30)
+entry_username.place(x=110, y=160)
 entry_username.focus()
 
 
-password_text = Label(root, text='Senha:', fg=color1)
+error_label_username = Label(root, fg='red')
+error_label_username.place(x=30, y=180)
+
+
+password_text = Label(root, text='Senha:', font=('Arial', 15, 'bold'),fg=color1, bg=color3)
 password_text.place(x=30, y=200)
-entry_password = Entry(root, show='*')
-entry_password.place(x=90, y=200)
+entry_password = Entry(root, show='*', width=30)
+entry_password.place(x=110, y=200)
+entry_password.bind("<Return>", confirm)
 
-error_label = Label(root, fg='red')
-error_label.place(x=30, y=270)
+error_label_password = Label(root, fg='red')
+error_label_password.place(x=30, y=270)
 
-login_button = Button(root, command=confirm, text="Login", bg=color4)
+# Criar botão com a imagem redimensionada
+login_button = Button(root, command=confirm, text="Login")
 login_button.place(x=30, y=230)
 
 root.mainloop()
